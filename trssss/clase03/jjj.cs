1d9467ff1a31628a8436d55eedbcfe53e9ce689e
maytetorrico-ops <mayteabigailflorestorrico@gmail.com>
2026-08-20 15:28:34 -0400

    init3
    jj

---
+++ trssss/clase03/jjj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trssss.clase03
+{
+    public class jjj
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola soy mayte tengo 18 años");
+        }
+    }
+}
