da045b0c2751fc5a533169b3fa4a87217062c5f2
maytetorrico-ops <mayteabigailflorestorrico@gmail.com>
2026-08-20 15:18:16 -0400

    init_1

--- trssss/Program.cs
+++ trssss/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace trssss
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
---
+++ trssss/clase02/uno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trssss.clase02
+{
+    public class uno
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola soy mayte tengo 19 años");
+        }
+    }
+}
